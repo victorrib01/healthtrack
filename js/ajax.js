61d67430e0e7cac252d5ca1cb61db5a5ccd68a91
Victor Ribeiro <vvictorrib@gmail.com>
2019-08-05 15:24:35 -0300

    Arrumado bugs e adicionado a parte de vizualizar tabelas

--- js/ajax.js
+++ js/ajax.js
@@ -1,25 +1,30 @@
-//Cadastro
+
 $(document).ready(function(){
+    //Cadastro
     $("#cadastrarPeso").click(function(){  
         $('#ajax-content').load("../ajax/peso/cadastrarPeso.html");
         $("#cadastrarPeso").hide();
         $("#vizualizarPeso").hide();
-        
    });
-});
-
-$(document).ready(function(){
-    $("#cadastrarAlimentacao").click(function(){  
+   $("#cadastrarAlimentacao").click(function(){  
         $('#ajax-content').load("../ajax/alimentacao/cadastrarAlimentacao.html");
         $("#cadastrarAlimentacao").hide();
         $("#vizualizarAlimentacao").hide();
-   });
-});
-
-$(document).ready(function(){
+    });
     $("#cadastrarPressao").click(function(){  
         $('#ajax-content').load("../ajax/pressaoArterial/cadastrarPressao.html");
         $("#cadastrarPressao").hide();
         $("#vizualizarPressao").hide();
    });
+   //Vizualizar
+   $("#vizualizarPeso").click(function(){  
+        $('#ajax-content').load("../ajax/peso/vizualizarPeso.html");
+        $("#cadastrarPeso").hide();
+        $("#vizualizarPeso").hide();
+    });
+    $("#vizualizarAlimentacao").click(function(){  
+        $('#ajax-content').load("../ajax/alimentacao/vizualizarAlimentacao.html");
+        $("#cadastrarAlimentacao").hide();
+        $("#vizualizarAlimentacao").hide();
+    });
 });
